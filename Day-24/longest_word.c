830effa67d8ea26278f28bb97336753e76c343dc
Manika Dwivedi <manikadwivedi4@gmail.com>
2026-06-29 16:08:55 +0530

    Changes

---
+++ Day-24/longest_word.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[200], word[50], longest[50];
+    int i = 0, j = 0, maxLen = 0;
+
+    printf("Enter a sentence: ");
+    fgets(str, sizeof(str), stdin);
+
+    while(1) {
+        if(str[i] != ' ' && str[i] != '\0' && str[i] != '\n') {
+            word[j++] = str[i];
+        }
+        else {
+            word[j] = '\0';
+
+            if(strlen(word) > maxLen) {
+                maxLen = strlen(word);
+                strcpy(longest, word);
+            }
+
+            j = 0;
+
+            if(str[i] == '\0' || str[i] == '\n')
+                break;
+        }
+
+        i++;
+    }
+
+    printf("Longest word: %s\n", longest);
+    printf("Length: %d\n", maxLen);
+
+    return 0;
+}
